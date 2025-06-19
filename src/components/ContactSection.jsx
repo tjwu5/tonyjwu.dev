62f4ab5b74e34b9ddaa5c8a2c02befee40b20426
tjwu5 <totowu9@gmail.com>
2025-06-19 16:02:50 -0700

    started and finished footer section, made slight adjustments to theme toggle so it fits

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -3,6 +3,7 @@ import { cn } from '@/lib/utils';
 
 
 export const ContactSection = () => {
+    
     return <section id="contact" className="py-24 px-4 relative bg-secondary">
         <div className="container mx-auto max-w-5xl">
             <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
